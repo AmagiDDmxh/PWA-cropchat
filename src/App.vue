d8123e235a624dc65b92980459f4b1ae1d16b215
AmagiDDmxh <1107537248@qq.com>
2017-04-26 22:51:31 +0800

    Add material-design-lite theme

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,22 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  require('material-design-lite')
+
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://code.getmdl.io/1.3.0/material.blue-red.min.css');
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
